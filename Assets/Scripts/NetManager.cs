9f7bb26b26b1430a300057d12c1bbd550bf3a46a
Patryk Szylin <patrykszylin93@gmail.com>
2019-02-20 22:16:30 +0000

    Initial networking state. Player movement

---
+++ Assets/Scripts/NetManager.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class NetManager : NetworkManager {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public override void OnClientConnect(NetworkConnection conn)
+    {
+        base.OnClientConnect(conn);
+
+        Debug.Log("Client connected...");
+        Debug.Log(conn.address);
+    }
+
+    public override void OnServerAddPlayer(NetworkConnection conn, short playerControllerId)
+    {
+        base.OnServerAddPlayer(conn, playerControllerId);
+    }
+
+    public override void OnServerConnect(NetworkConnection conn)
+    {
+        base.OnServerConnect(conn);
+
+        Debug.Log("Connected new player: " + conn.address);
+    }
+
+    public override void OnStartHost()
+    {
+        base.OnStartHost();
+       
+    }
+
+
+    public override void OnStartClient(NetworkClient client)
+    {
+        base.OnStartClient(client);
+
+
+    }
+
+
+    public override void OnStartServer()
+    {
+        base.OnStartServer();
+        
+    }
+}
---
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class PlayerMovement : NetworkBehaviour
+{
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+
+        if (!isLocalPlayer)
+            return;
+        Move();
+    }
+
+    public void Move()
+    {
+        //if (!isLocalPlayer)
+        //    return;
+
+        if (Input.GetKey(KeyCode.W))
+        {
+            Debug.Log("djasiojdi");
+            this.transform.Translate(Vector3.forward * Time.deltaTime * 10f);
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            Debug.Log("djasiojdi");
+            this.transform.Translate(Vector3.back * Time.deltaTime * 10f);
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            Debug.Log("djasiojdi");
+            this.transform.Translate(Vector3.left * Time.deltaTime * 10f);
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            Debug.Log("djasiojdi");
+            this.transform.Translate(Vector3.right * Time.deltaTime * 10f);
+        }
+    }
+
+
+}
